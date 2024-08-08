6e09e7cf1b5ae57ad4816592295e21e5c5e57b41
Axel <axstraube@gmail.com>
2024-08-07 20:20:32 -0700

    Start Credits component

---
+++ src/components/Credits.js
@@ -0,0 +1,9 @@
+function Credits() {
+    return (
+        <ul>
+            <li>Video by Magda Ehlers: https://www.pexels.com/video/nature-weather-leaves-rain-4065288/</li>
+        </ul>
+    )
+}
+
+export default Credits;
